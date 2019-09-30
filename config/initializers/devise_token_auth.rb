a7dc1a3592a69eaf0cefc4bfd9ec7c734e5e7c88
Juan Aparicio <apariciojuan30@gmail.com>
2019-09-30 14:47:24 -0300

    Added reset password functionality

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -3,7 +3,7 @@ DeviseTokenAuth.setup do |config|
   config.default_password_reset_url = ENV.fetch('PASSWORD_RESET_URL')
   config.enable_standard_devise_support = true
   config.token_lifespan = 2.years
-  config.batch_request_buffer_throttle = 10.seconds
+  config.batch_request_buffer_throttle = 60.minutes
   config.change_headers_on_each_request = false
   config.max_number_of_devices = 5
 end
