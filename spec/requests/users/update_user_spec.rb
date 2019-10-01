a2f4f589716339e684f55aaf47a510642ce6d88c
Juan Aparicio <apariciojuan30@gmail.com>
2019-10-01 12:01:29 -0300

    Added valid and invalid values to the test

--- spec/requests/users/update_user_spec.rb
+++ spec/requests/users/update_user_spec.rb
@@ -8,7 +8,7 @@ describe 'PATCH /api/v1/users/', type: :request do
 
   context 'with logged-out user' do
     let(:header_param) { {} }
-    let(:user_param) { create :user }
+    let(:user_param) { build :user }
 
     it 'returns an error code' do
       update_registration
@@ -29,23 +29,53 @@ describe 'PATCH /api/v1/users/', type: :request do
     let!(:header_param) { auth_headers(user) }
 
     context 'with correct params' do
-      let!(:user_param) do
-        {
-          first_name: user.first_name,
-          last_name: user.last_name,
-          gender: user.gender
-        }
+      context 'with valid values' do
+        let!(:user_param) do
+          {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            gender: user.gender
+          }
+        end
+
+        it 'returns a success code' do
+          update_registration
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'returns the updated user' do
+          update_registration
+          res = parsed_response
+          expect(res).to include_json(data: user_param)
+        end
       end
 
-      it 'returns a success code' do
-        update_registration
-        expect(response).to have_http_status(:success)
-      end
+      context 'with invalid values' do
+        let!(:user_param) do
+          {
+            first_name: 'first_name_that_is_too_long_and_not_accepted',
+            last_name: 'last_name_that_is_too_long_and_not_accepted',
+            gender: 'invalid_gender'
+          }
+        end
 
-      it 'returns the updated user' do
-        update_registration
-        res = parsed_response
-        expect(res).to include_json(data: user_param)
+        it 'returns an error code' do
+          update_registration
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'returns the error message' do
+          update_registration
+          res = parsed_response
+          expected_response = {
+            errors: {
+              gender: ['is not included in the list'],
+              first_name: ['is too long (maximum is 20 characters)'],
+              last_name: ['is too long (maximum is 20 characters)']
+            }
+          }
+          expect(res).to include_json(expected_response)
+        end
       end
     end
 
