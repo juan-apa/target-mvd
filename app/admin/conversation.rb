40b75542ac6570275869cfc80c32fc893aacc6a6
Juan Aparicio <apariciojuan30@gmail.com>
2019-10-21 17:00:11 -0300

    changes for rubocop validation

--- app/admin/conversation.rb
+++ app/admin/conversation.rb
@@ -4,11 +4,10 @@ ActiveAdmin.register Conversation do
   show do
     panel 'Messages' do
       table_for(conversation.messages) do |t|
-        t.column('User') { |message| message.user.email }
+        t.column(:user)
         t.column(:body)
         t.column(:created_at)
       end
     end
   end
 end
-
